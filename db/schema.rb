2c2147d43bb2509439e158ca553c06e9e9f26eff
David Donohue <donohue76@hotmail.com>
2016-04-01 10:17:19 -0700

    added accept:boolean to barters table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401042728) do
+ActiveRecord::Schema.define(version: 20160401171607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160401042728) do
     t.string   "neighborhood"
     t.integer  "user_id"
     t.text     "interested_in"
+    t.boolean  "accept"
   end
 
   add_index "barters", ["user_id"], name: "index_barters_on_user_id", using: :btree

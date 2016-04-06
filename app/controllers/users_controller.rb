9b8e44c454858b815864950530f2c986c863f1e9
David Donohue <donohue76@hotmail.com>
2016-04-06 12:11:31 -0700

    removed cancancan and created unauthorized page with routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
-    #before_action :set_user, only: [:show, :edit, :update, :destroy]
-    load_and_authorize_resource
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
   def index
@@ -11,17 +10,22 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-
+    if current_user.id == params[:id].to_i
+      # show current user
+    else
+      # unauthorized access
+      render 'users/unauthorized'
+    end
   end
 
 
   def profile
-     @user = User.find(current_user.id)
+    @user = User.find(current_user.id)
   end
 
   # GET /users/new
   def new
-    #@user = User.new
+    @user = User.new
   end
 
   # GET /users/1/edit
@@ -31,7 +35,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    #@user = User.new(user_params)
+    @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
@@ -70,9 +74,9 @@ class UsersController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_user
-    #   @user = User.find(params[:id])
-    # end
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
